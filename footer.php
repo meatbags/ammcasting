f1bbe50ebc0302751b99ac621c9c16d05ca63716
meatbags <jxburrow@gmail.com>
2019-09-06 19:31:02 +1000

    logo

--- footer.php
+++ footer.php
@@ -4,6 +4,7 @@
       <a href='https://au.linkedin.com/in/annie-murtagh-monks-244a0818' target='_blank'><img src='<?php echo get_template_directory_uri(); ?>/img/linkedin-logo.png'></a>
       <a href='https://www.imdb.com/name/nm0615426/' target='_blank'><img src='<?php echo get_template_directory_uri(); ?>/img/imdb-logo.png'></a>
       <a href='https://pro.imdb.com/name/nm0615426/' target='_blank'><img src='<?php echo get_template_directory_uri(); ?>/img/imdb-pro-logo.png'></a>
+      <a href='https://bsstc.com.au/' target='_blank'><img src='<?php echo get_template_directory_uri(); ?>/img/black-swan-logo.png'></a>
     </div>
     <div class='footer__copyright'>
       &copy; <?php echo date('Y'); ?> Annie Murtagh-Monks Casting<br />
